078e6132bef470d1f1ca64edf10e914c0bc3be71
twan65 <cx.twan65@gmail.com>
2020-02-16 17:48:46 +0900

    [update] if

--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,17 @@
 package main
 
 func main() {
-	var k int = 10
-	var p = &k  // Kのアドレスを指す。
-	println(*p) // pが指しているアドレスの実際値。
+	var k int = 1
+	if k == 1 { // {が同じライン
+		println("One")
+	}
+
+	k = 3
+	if k == 1 {
+		println("One")
+	} else if k == 2 { // {が同じライン
+		println("Two")
+	} else { // {が同じライン
+		println("Other")
+	}
 }
