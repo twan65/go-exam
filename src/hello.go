f168403b1793be37c3bef3a110f178785e0d5a88
twan65 <cx.twan65@gmail.com>
2020-02-16 15:31:48 +0900

    [update] 変数

--- src/hello.go
+++ src/hello.go
@@ -1,5 +1,22 @@
 package main
 
 func main() {
-	println("Hello world")
+	const a int = 10
+	const b = 20
+	const s = "Hi"
+	const f = 11.
+
+	const (
+		Apple = iota
+		Grape
+		Orange
+	)
+
+	println(a)
+	println(b)
+	println(s)
+	println(f)
+	println(Apple)
+	println(Grape)
+	println(Orange)
 }
