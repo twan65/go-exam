954a9722993aa436a87018e5b629901c1b7b1530
twan65 <cx.twan65@gmail.com>
2020-02-16 17:58:27 +0900

    [update]  Pass By Value, Pass By Reference

--- src/hello.go
+++ src/hello.go
@@ -1,24 +1,16 @@
 package main
 
 func main() {
-	sum := 0
-	for i := 1; i <= 100; i++ {
-		sum += i
-	}
-	println(sum)
+	msg := "world"
+	say1(msg)  // Pass By Value
+	say2(&msg) // Pass By Reference
+}
 
-	n := 1
-	for n < 100 {
-		n *= 2
-		//if n > 90 {
-		//   break
-		//}
-	}
-	println(n)
+func say1(msg string) {
+	println("Hello, " + msg)
+}
 
-	names := []string{"홍길동", "이순신", "강감찬"}
-
-	for index, name := range names {
-		println(index, name)
-	}
+func say2(msg *string) {
+	*msg = "Anveloper"
+	println(*msg)
 }
