a4a7c64e5c996a2ea31c7713426e8487471036a3
twan65 <cx.twan65@gmail.com>
2020-02-16 18:06:36 +0900

    [update] Function

--- src/hello.go
+++ src/hello.go
@@ -1,16 +1,39 @@
 package main
 
 func main() {
-	msg := "world"
-	say1(msg)  // Pass By Value
-	say2(&msg) // Pass By Reference
+	say("This", "is", "a", "book")
+	say("Hi")
+
+	total := sum1(1, 2, 3, 4, 5, 6, 7)
+	println(total)
+
+	sum, cnt := getSumAndCnt(5, 4, 12, 32, 12)
+	println(sum, cnt)
 }
 
-func say1(msg string) {
-	println("Hello, " + msg)
+func say(msg ...string) {
+	for _, s := range msg { // "_"はIndexを使用しないという意味。
+		println(s)
+	}
 }
 
-func say2(msg *string) {
-	*msg = "Anveloper"
-	println(*msg)
+func sum1(nums ...int) int {
+	s := 0
+
+	for _, num := range nums {
+		s += num
+	}
+	return s
+}
+
+func getSumAndCnt(nums ...int) (int, int) {
+	s := 0
+	cnt := 0
+
+	for _, num := range nums {
+		s += num
+		cnt++
+	}
+
+	return s, cnt
 }
