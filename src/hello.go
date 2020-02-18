ab9c871e1da8acd0b548e7f3340e173676b4d907
twan65 <cx.twan65@gmail.com>
2020-02-18 22:17:47 +0900

    [update] interface

--- src/hello.go
+++ src/hello.go
@@ -1,17 +1,11 @@
 package main
 
-//Rect - struct定義
-type Rect struct {
-	width, height int
-}
-
-//Rectのarea()メソッド
-func (r Rect) area() int {
-	return r.width * r.height
-}
-
 func main() {
-	rect := Rect{10, 20} // struct生成
-	area := rect.area()  // メソッドの呼び出し
-	println(area)
+	var a interface{} = 1
+
+	i := a
+	j := a.(int)
+
+	println(i)
+	println(j)
 }
