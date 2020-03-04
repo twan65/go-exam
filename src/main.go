e8f4407a14d7edaf5b93cde4426ed0349b4661a0
twan65 <cx.twan65@gmail.com>
2020-03-04 23:10:15 +0900

    [update] サードパーティ製のマルチプレクサ

--- src/main.go
+++ src/main.go
@@ -3,28 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"reflect"
-	"runtime"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called - " + name)
-		h(w, r)
-	}
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "helllo, %s\n", p.ByName("name"))
 }
 
 func main() {
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
-
-	http.HandleFunc("/hello", log(hello))
-
 	server.ListenAndServe()
 }
