2bc9beeac8b7478effd870681c9da8c59626749c
twan65 <cx.twan65@gmail.com>
2020-03-05 22:00:53 +0900

    [update] Request Header確認

--- src/main.go
+++ src/main.go
@@ -3,21 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "helllo, %s\n", p.ByName("name"))
+func header(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
 }
 
 func main() {
-	mux := httprouter.New()
-	mux.GET("/hello/:name", hello)
 
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: mux,
+		Addr: "127.0.0.1:8080",
 	}
+
+	http.HandleFunc("/header", header)
 	server.ListenAndServe()
 }
